273df3b5969e4da0000863c7dfa5e5f47c49dcd2
Hektor-alfatih <160695682+Hektor-alfatih@users.noreply.github.com>
2024-07-30 11:32:07 +0700

    update

--- meeting 1/test.js
+++ meeting 1/test.js
@@ -1,1 +1,21 @@
-document.write("mau belajar javascript");
+
+var a;
+console.log(a);
+
+
+var nama = ("hektor ");
+var umur = ("15 thn ");
+var tempattinggal = ("Rumpin bogor");
+var hasil = (nama + umur + tempattinggal);
+console.log(hasil);
+
+
+var b = null;
+console.log (b);
+
+
+
+var alas = 5;
+var tinggi = 7;
+var rumus = 0.5;
+console.log(alas * tinggi * rumus);
